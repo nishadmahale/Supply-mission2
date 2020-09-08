eaf22b08a957dcac9954e49e4316743aea7dbef7
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-09-08 17:07:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,11 +58,19 @@ function keyPressed() {
  if (keyCode === DOWN_ARROW) {
 	// Look at the hints in the document and understand how to make the package body fall only on
 	function keyPressed() { if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false); } }
-function keyPressed() { if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false); } }
 
-
+if (keyCode === LEFT_ARROW) { 
+	  helicopterSprite.x=helicopterSprite.x-20; translation={x:-20,y:0} 
+	  Matter.Body.translate(packageBody, translation) 
+	}
+   else if (keyCode === RIGHT_ARROW) { 
+	   helicopterSprite.x=helicopterSprite.x+20; translation={x:20,y:0} 
+	   Matter.Body.translate(packageBody, translation) 
+	} 
+   else if (keyCode === DOWN_ARROW) { 
+	   Matter.Body.setStatic(packageBody,false); } 
     
-  }
+  
 }
 
 
