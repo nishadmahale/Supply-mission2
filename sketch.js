1d102bd3fa89679cf890854e66fedf24751dabc2
nishadmahale <70054272+nishadmahale@users.noreply.github.com>
2020-09-11 19:09:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const Body = Matter.Body;
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	packageIMG=loadImage("package.png");
 }
 
 function setup() {
@@ -17,15 +17,16 @@ function setup() {
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
 
 	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	groundSprite.shapeColor=color(255);
+
 
 
 	engine = Engine.create();
@@ -48,35 +49,43 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
 
-packageSprite.x= packageBody.position.x ;
-packageSprite.y= packageBody.position.y ;
+  if(keyDown(DOWN_ARROW)) {
+
+	Matter.Body.setStatic(packageBody,false); 
+	restitution=1.7;
+
+  }
+
+  
+
  
-	
-	
-	
-	
+ 
   drawSprites();
  
 }
 
 function keyPressed(packageBody) {
- if (keyCode === DOWN_ARROW) {
+ if (keyDown === DOWN_ARROW) {
 	// Look at the hints in the document and understand how to make the package body fall only on
 	function keyPressed() { if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false); } }
-
-if (keyCode === LEFT_ARROW) { 
+function keyPressed() { if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false); } }
+    
+  }
+   if (keyDown === LEFT_ARROW) { 
 	  helicopterSprite.x=helicopterSprite.x-20; translation={x:-20,y:0} 
 	  Matter.Body.translate(packageBody, translation) 
 	}
-   else if (keyCode === RIGHT_ARROW) { 
+   else if (keyDown === RIGHT_ARROW) { 
 	   helicopterSprite.x=helicopterSprite.x+20; translation={x:20,y:0} 
 	   Matter.Body.translate(packageBody, translation) 
 	} 
-   else if (keyCode === DOWN_ARROW) { 
+   else if (keyDown === DOWN_ARROW) { 
 	   Matter.Body.setStatic(packageBody,false); } 
-    
-  
+
+
 }
 
 
